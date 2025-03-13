c37ebfa53027763e50f04b122a73b200f9f496d8
Bhavna1312 <bhavnarajput973@gmail.com>
2025-03-13 23:33:08 +0530

    Initial commit: Added React project files

---
+++ first-app/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <img src="harry.jpg" alt="Harry Potter" />
+      </div>
+      <div className="">Harry</div>
+    </>
+  );
+}
+
+export default App;
