8dadd2ba067de81854dff2bd032e5614a59ab9bb
Bhavna1312 <bhavnarajput973@gmail.com>
2025-03-18 23:41:26 +0530

    Updated App component to include Navbar and Footer

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,8 +1,10 @@
-import Navbar from "./components/navbar";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
