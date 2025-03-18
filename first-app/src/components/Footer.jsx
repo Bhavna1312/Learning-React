3756485ace40923b67192f780b47bd42c06c60e0
Bhavna1312 <bhavnarajput973@gmail.com>
2025-03-18 23:20:31 +0530

    Updated App and Footer components and styles

--- first-app/src/components/Footer.jsx
+++ first-app/src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div>copyright codewithharry.com | All rights reserved</div>;
+  return (
+    <div className="footer">
+      copyright codewithharry.com | All rights reserved
+    </div>
+  );
 };
 
 export default Footer;
