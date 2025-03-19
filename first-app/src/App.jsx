a23c6cd77d5513905890711655fae5c67a18c152
Bhavna1312 <bhavnarajput973@gmail.com>
2025-03-19 22:47:41 +0530

    Updated App.jsx and Card.jsx

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,10 +1,13 @@
+import Card from "./components/Card";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
       <Navbar />
-      <div className="cards"></div>
+      <div className="cards">
+        <Card />
+      </div>
       <Footer />
     </>
   );
