8125472d9b9a98f4e77dfda5587bf442dfcac63b
Bhavna1312 <bhavnarajput973@gmail.com>
2025-03-21 23:37:03 +0530

    Updated Card.jsx to include new functionality or fixes

--- first-app/src/components/Card.jsx
+++ first-app/src/components/Card.jsx
@@ -4,6 +4,11 @@ import "./Card.css";
 const Card = (props) => {
   return (
     <div className="card">
+      <img
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fcoding&psig=AOvVaw1n4X3OoaDA4aTntwgy50O4&ust=1742666580992000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMi_iovhm4wDFQAAAAAdAAAAABAJ"
+        alt=""
+        width=""
+      />
       <h1>{props.title}</h1>
       <p>{props.description}</p>
     </div>
