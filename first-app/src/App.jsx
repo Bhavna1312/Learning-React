0ad5a025c1b86b78e00c23031787369f9d94c282
Bhavna1312 <bhavnarajput973@gmail.com>
2025-03-21 23:26:56 +0530

    Updated App.jsx to include new functionality

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -8,9 +8,8 @@ function App() {
       <div className="cards">
         <Card title="card 1" description="card 1 desc" />
         <Card title="card 2" description="card 2 desc" />
-        <Card title="card 1" description="card 2 desc" />
-        <Card title="card 1" description="card 2 desc" />
-        <Card title="card 1" description="card 2 desc" />
+        <Card title="card 3" description="card 3 desc" />
+        <Card title="card 4" description="card 4 desc" />
       </div>
       <Footer />
     </>
