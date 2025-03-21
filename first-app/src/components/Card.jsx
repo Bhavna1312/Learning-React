0c9bd878e4e30abd031ad4dc3274a13bd989b4b3
Bhavna1312 <bhavnarajput973@gmail.com>
2025-03-21 23:38:51 +0530

    Updated Card.jsx to include new functionality or fixes

--- first-app/src/components/Card.jsx
+++ first-app/src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
   return (
     <div className="card">
       <img
-        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fcoding&psig=AOvVaw1n4X3OoaDA4aTntwgy50O4&ust=1742666580992000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMi_iovhm4wDFQAAAAAdAAAAABAJ"
+        src="https://www.shutterstock.com/image-photo/igniting-innovation-harnessing-power-coding-600nw-2425426569.jpg"
         alt=""
         width=""
       />
