080d9b676148a858af57215284aab46b7f275a88
Bhavna1312 <bhavnarajput973@gmail.com>
2025-03-22 22:51:06 +0530

    Updated Card component styles and logic

--- first-app/src/components/Card.jsx
+++ first-app/src/components/Card.jsx
@@ -8,7 +8,7 @@ const Card = (props) => {
         src="https://www.shutterstock.com/image-photo/igniting-innovation-harnessing-power-coding-600nw-2425426569.jpg"
         alt=""
         width={230}
-        style={{ border: "2px solid red" }}
+        style={{ border: "2px solid black" }}
       />
       <h1>{props.title}</h1>
       <p>{props.description}</p>
